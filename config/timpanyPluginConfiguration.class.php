443ff06b0e626d2e80f0e4e5bc54337878098841
Thomas Kappel <quafzi@netextreme.de>
2010-10-10 14:47:55 +0200

    initialized fresh plugin

---
+++ config/timpanyPluginConfiguration.class.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * timpanyPlugin configuration.
+ * 
+ * @package     timpanyPlugin
+ * @subpackage  config
+ * @author      The Timpany Core Team <lsdug@googlegroups.com>
+ * @version     SVN: $Id: PluginConfiguration.class.php 17207 2009-04-10 15:36:26Z Kris.Wallsmith $
+ */
+class timpanyPluginConfiguration extends sfPluginConfiguration
+{
+  const VERSION = '1.0.0-DEV';
+
+  /**
+   * @see sfPluginConfiguration
+   */
+  public function initialize()
+  {
+  }
+}
---
+++ test/bootstrap/functional.php
@@ -0,0 +1,21 @@
+<?php
+
+if (!isset($app))
+{
+  $app = 'frontend';
+}
+
+require_once $_SERVER['SYMFONY'].'/autoload/sfCoreAutoload.class.php';
+sfCoreAutoload::register();
+
+function timpanyPlugin_cleanup()
+{
+  sfToolkit::clearDirectory(dirname(__FILE__).'/../fixtures/project/cache');
+  sfToolkit::clearDirectory(dirname(__FILE__).'/../fixtures/project/log');
+}
+timpanyPlugin_cleanup();
+register_shutdown_function('timpanyPlugin_cleanup');
+
+require_once dirname(__FILE__).'/../fixtures/project/config/ProjectConfiguration.class.php';
+$configuration = ProjectConfiguration::getApplicationConfiguration($app, 'test', isset($debug) ? $debug : true);
+sfContext::createInstance($configuration);
