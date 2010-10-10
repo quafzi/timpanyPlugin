fc5b554eca56d20f9335d9d1759a8a80b08f3241
Frank Weigmann <sunrise2k5@googlemail.com>
2010-10-10 17:36:28 +0200

    forms and filters

---
+++ lib/filter/doctrine/PlugintpyTaxClassFormFilter.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * PlugintpyTaxClass form.
+ *
+ * @package    ##PROJECT_NAME##
+ * @subpackage filter
+ * @author     ##AUTHOR_NAME##
+ * @version    SVN: $Id: sfDoctrineFormFilterPluginTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+abstract class PlugintpyTaxClassFormFilter extends BasetpyTaxClassFormFilter
+{
+}
---
+++ lib/form/doctrine/PlugintpyOrderStateForm.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * PlugintpyOrderState form.
+ *
+ * @package    ##PROJECT_NAME##
+ * @subpackage form
+ * @author     ##AUTHOR_NAME##
+ * @version    SVN: $Id: sfDoctrineFormPluginTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+abstract class PlugintpyOrderStateForm extends BasetpyOrderStateForm
+{
+}
