cc7f0d6e86c7449d2c97f0799f555f4e58329c45
Thomas Kappel <quafzi@netextreme.de>
2010-10-11 19:18:11 +0200

    moved method retrieveByIdAndRegion from PlugintpyTaxClassTable to PlugintpyTaxTable

--- lib/model/doctrine/PlugintpyTaxClassTable.class.php
+++ lib/model/doctrine/PlugintpyTaxClassTable.class.php
@@ -17,16 +17,4 @@ class PlugintpyTaxClassTable extends Doctrine_Table
     {
         return Doctrine_Core::getTable('PlugintpyTaxClass');
     }
-
-    /**
-     * return tax by class id and region
-     * @param int $taxClassId
-     * @param string $region
-     * @return Doctine_Collection
-     */
-    public function retrieveByIdAndRegion($taxClassId, $region) {
-      $taxClassId = intval($taxClassId);
-
-      return $this->createQuery()->where("tax_class_id = ? AND region = ?", array($taxClassId, $region))->execute();
-    }
 }
--- lib/model/doctrine/PlugintpyTaxTable.class.php
+++ lib/model/doctrine/PlugintpyTaxTable.class.php
@@ -16,4 +16,16 @@ class PlugintpyTaxTable extends Doctrine_Table
     {
         return Doctrine_Core::getTable('PlugintpyTax');
     }
+
+    /**
+     * return tax by class id and region
+     * @param int $taxClassId
+     * @param string $region
+     * @return Doctine_Collection
+     */
+    public function retrieveByIdAndRegion($taxClassId, $region) {
+      $taxClassId = intval($taxClassId);
+
+      return $this->createQuery()->where("tax_class_id = ? AND region = ?", array($taxClassId, $region))->execute();
+    }
 }
